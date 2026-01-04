984c41ddb6e1e1079f944d0c17250b8b7c458967
DZultra <DanielZ123@web.de>
2026-01-04 19:21:04 +0100

    Added @SerializedName Annotation to avatar String in PlayerSearchResult

--- src/main/java/net/dzultra/jfa/responses/PlayerSearchResponse.java
+++ src/main/java/net/dzultra/jfa/responses/PlayerSearchResponse.java
@@ -1,7 +1,12 @@
 package net.dzultra.jfa.responses;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.util.List;
 
 public record PlayerSearchResponse(List<PlayerSearchResult> results) implements Response{
-    public record PlayerSearchResult(String uuid, String username, String avatar) {}
+    public record PlayerSearchResult(
+            String uuid,
+            String username,
+            @SerializedName("avatar") String headUrl) {}
 }

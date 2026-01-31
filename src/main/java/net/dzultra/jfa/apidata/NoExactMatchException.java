51a4c15eecf29c5941428247c3c9b446304e9314
DZultra <DanielZ123@web.de>
2026-01-31 19:18:35 +0100

    Made NoExactMatchException protected

--- src/main/java/net/dzultra/jfa/apidata/NoExactMatchException.java
+++ src/main/java/net/dzultra/jfa/apidata/NoExactMatchException.java
@@ -1,7 +1,7 @@
 package net.dzultra.jfa.apidata;
 
 public class NoExactMatchException extends RuntimeException {
-    public NoExactMatchException(PlayerSearch playerSearch) {
+    protected NoExactMatchException(PlayerSearch playerSearch) {
         super("No exact name match found in PlayerSearch for '" + playerSearch.getRequestedUsername() + "' in " + playerSearch.response.toString());
     }
 }
